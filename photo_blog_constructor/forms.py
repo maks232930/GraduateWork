d74fae964214f89a3a206124c234aec5924050b0
Maksim Pukshlis <maksimpusklis7@gmail.com>
2021-02-13 17:09:06 +0300

    created form register

--- photo_blog_constructor/forms.py
+++ photo_blog_constructor/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from users.models import User
 from .models import Comment
 
 
@@ -13,3 +14,26 @@ class CommentForm(forms.ModelForm):
             'reader': forms.HiddenInput(),
             'post': forms.HiddenInput()
         }
+
+
+class ReaderRegistrationForm(forms.ModelForm):
+    password = forms.CharField(max_length=255, label='Пароль',
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
+    password2 = forms.CharField(max_length=255, label='Повторите пароль',
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Повторите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name')
+        widgets = {
+            'email': forms.TextInput(attrs={'placeholder': 'Email'}),
+            'username': forms.TextInput(attrs={'placeholder': 'Имя пользователя'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'Имя'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Фамилия'}),
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают!!')
+        return cd['password2']
